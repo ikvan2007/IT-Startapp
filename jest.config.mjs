489d0de1b6991e20148c426033f54fa4979676c5
altair <hakage47@gmail.com>
2026-06-09 23:39:38 +0600

    фикс тестов

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,3 @@
-// jest.config.mjs
 import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({ dir: './' })
@@ -6,20 +5,22 @@ const createJestConfig = nextJest({ dir: './' })
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEach: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: [
-    '<rootDir>/__tests__/**/*.test.ts',
-    '<rootDir>/__tests__/**/*.test.tsx',
+    '<rootDir>/_tests_/**/*.test.ts',
+    '<rootDir>/_tests_/**/*.test.tsx',
     '<rootDir>/src/**/*.test.ts',
     '<rootDir>/src/**/*.test.tsx',
   ],
   collectCoverageFrom: [
-    'src/lib/**/*.ts',
-    'src/app/api/**/*.ts',
+    'src/**/*.ts',
+    'src/**/*.tsx',
     '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.test.tsx',
   ],
   coverageReporters: ['text', 'lcov', 'html'],
   coverageThreshold: {
@@ -30,8 +31,7 @@ const config = {
       statements: 70,
     },
   },
-  // Показывать каждый тест в verbose
-  verbose: false,
+  verbose: true,
 }
 
 export default createJestConfig(config)
